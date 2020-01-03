37f93c43b2aa60306b9d7f66155327602d3c8948
AdamSiekierski <adam.siekiera@outlook.com>
2020-01-03 22:10:33 +0100

    Enable cors

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './modules/app.module';
 
 NestFactory.create(AppModule).then(app => {
+  app.enableCors();
   const port = process.env.PORT || 3000;
   app.listen(port).then(() => {
     console.log(`App listening at port ${port}`);
